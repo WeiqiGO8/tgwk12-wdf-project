cb28eecba88021ce5105517e91e9b1009f69f5b3
Hannah Jansson <hannah.lingzhi@gmail.com>
2024-09-16 12:01:06 +0200

    working on layout and organizing the css structure

--- model.js
+++ model.js
@@ -1,24 +1,102 @@
 // MODEL
-const projects=[
-  { "id":"1", "name":"Counting people with a camera", "type":"Research",
-  "desc":"The purpose of this project is to count people passing through a corridor and to know how many are in the room at a certain time.", "year": 2022,
-  "dev":"Python and OpenCV (Computer vision) library", "url":"/img/counting.png" },
+const projects = [
+	{
+		id: "1",
+		name: "A flower",
+		type: "photograph",
+		desc: "A flower",
+		year: 2022,
+		dev: "-",
+		url: "/img/flower.jpg",
+	},
 
-  { "id":"2", "name":"Visualisation of 3D medical images", "type":"Research",
-  "desc":"The project makes a 3D model of the analysis of the body of a person and displays the detected health problems. It is useful for doctors to view in 3D their patients and the evolution of a disease.", "year": 2012,
-  "url":"/img/medical.png" },
+	{
+		id: "2",
+		name: "forest image",
+		type: "photograph",
+		desc: "a forest image",
+		year: 2023,
+		url: "/img/forest.jpg",
+	},
 
-  { "id":"3", "name":"Multiple questions system", "type":"Teaching",
-  "desc":"During the lockdowns in France, this project was useful to test the  students online with a Quizz system.", "year": 2021, "url":"/img/qcm07.png" },
+	{
+		id: "3",
+		name: "holiday img",
+		type: "photograph",
+		desc: "holiday image.",
+		year: 2021,
+		url: "/img/holiday.jpg",
+	},
 
-  { "id":"4", "name":"Image comparison with the Local Dissmilarity Map",
-  "desc":"The project is about finding and quantifying the differences between two images of the same size. The applications were numerous: satellite maging, medical imaging,...", "year": 2020, "type":"Research", "url":"/img/diaw02.png" }, 
-  { "id":"5", "name":"Management system for students' internships", "desc":"This project was about the creation of a database to manage the students' internships.", "year": 2012, "type":"Teaching", "url":"/img/management.png" },
+	{
+		id: "4",
+		name: "rauk",
+		desc: "rauk",
+		year: 2020,
+		type: "photograph",
+		url: "/img/rauk.jpg",
+	},
 
-  { "id":"6", "name":"Magnetic Resonance Spectroscopy", "desc":"Analysis of signals and images from Magnetic Resonance Spectroscopy and Imaging.", "year":
-  2013, "type":"Research", "url":"/img/yu00.png" },
-  
-  { "id":"7", "name":"Signal Analysis for Detection of Epileptic Deseases",
-  "desc":"This project was about the detection of epileptic problems in signals", "year": 2019, "type":"research", "url":"/img/youssef00.png" },
-  ]
-  
+	{
+		id: "5",
+		name: "rauk",
+		desc: "-",
+		year: 2012,
+		type: "photograph",
+		url: "/img/rauk-2.jpg",
+	},
+
+	{
+		id: "6",
+		name: "sky",
+		desc: "-",
+		year: 2013,
+		type: "photograph",
+		url: "/img/sky-img.jpg",
+	},
+
+	{
+		id: "7",
+		name: "sky",
+		desc: "-",
+		year: 2019,
+		type: "photograph",
+		url: "/img/sky-2.jpg",
+	},
+
+	{
+		id: "8",
+		name: "sky",
+		desc: "-",
+		year: 2019,
+		type: "photograph",
+		url: "/img/sky-3.jpg",
+	},
+
+	{
+		id: "9",
+		name: "sky",
+		desc: "-",
+		year: 2019,
+		type: "photograph",
+		url: "/img/sky-3.jpg",
+	},
+
+	{
+		id: "10",
+		name: "sky",
+		desc: "-",
+		year: 2019,
+		type: "photograph",
+		url: "/img/sky-4.jpg",
+	},
+
+	{
+		id: "11",
+		name: "sky",
+		desc: "-",
+		year: 2019,
+		type: "photograph",
+		url: "/img/sky-5.jpg",
+	},
+];
